05784b698bcbe12ba805c7372ae509b0cc1bd07a
fatihkckrt <fatihkckrt@gmail.com>
2026-09-07 17:48:31 +0000

    fix: remove redirect script, add ErrorBoundary, deploy dual root and docs support

--- sw.js
+++ sw.js
@@ -90,7 +90,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "1297f4945bc714388cb5761d984d026d"
   }, {
     "url": "index.html",
-    "revision": "79043f99d189356d7afddf4ce33042a0"
+    "revision": "a61dae3f71ef74e26de586f73b4a7104"
   }, {
     "url": "favicon.ico",
     "revision": "42102188842781ecd501600044ac208d"
@@ -98,10 +98,10 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "980238664110ece9b88da0f98795f872"
   }, {
-    "url": "assets/index-DEJBupFi.js",
+    "url": "assets/index-d7D8yzqU.css",
     "revision": null
   }, {
-    "url": "assets/index-CAnE_RkB.css",
+    "url": "assets/index-VFIa7Q_e.js",
     "revision": null
   }, {
     "url": "apple-touch-icon.png",
