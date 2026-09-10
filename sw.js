6d56360cb7314f9bad40c190e08a02ea8c1e101c
fatihkckrt <fatihkckrt@gmail.com>
2026-09-10 19:38:28 +0000

    Auto-deploy: sync verified production build with self-healing

--- sw.js
+++ sw.js
@@ -90,7 +90,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "1297f4945bc714388cb5761d984d026d"
   }, {
     "url": "index.html",
-    "revision": "c55a731df5ea89b11ec664b0ce86f250"
+    "revision": "3f7d3349c122bce36fd67beec4cb26c6"
   }, {
     "url": "favicon.ico",
     "revision": "42102188842781ecd501600044ac208d"
@@ -98,10 +98,10 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "url": "apple-touch-icon.png",
     "revision": "980238664110ece9b88da0f98795f872"
   }, {
-    "url": "assets/index-J_7N-jKc.js",
+    "url": "assets/index-CHicyx57.css",
     "revision": null
   }, {
-    "url": "assets/index-CHicyx57.css",
+    "url": "assets/index-BlmcWcRi.js",
     "revision": null
   }, {
     "url": "apple-touch-icon.png",
